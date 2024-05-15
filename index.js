d3e38762411d1b3b8fa0275c3fb2dd836671ef8e
Muhammad Zawwar Akram <156500678+IbtahajAkram@users.noreply.github.com>
2024-05-15 12:50:59 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 4000;
-const connectionString = 'mongodb+srv://zawwar1313:SKemd6qGp8cvc3BT@cluster0.yf0z8hu.mongodb.net/user?retryWrites=true&w=majority';
-const databaseName = 'user';
-const SignupcollectionName = 'JobsSignup';
-const AllJobscollectionName = 'AllJobs';
-const ApplyJobscollectionName = 'ApplyJobs';
+const connectionString = '';
+const databaseName = '';
+const SignupcollectionName = '';
+const AllJobscollectionName = '';
+const ApplyJobscollectionName = '';
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 
